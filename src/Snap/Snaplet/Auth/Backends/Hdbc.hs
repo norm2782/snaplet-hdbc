487e6e15f31c48bc105ad771a8e82b4231d9daca
Jurriën Stutterheim <j.stutterheim@me.com>
2011-09-18 16:24:08 +0200

    Populate most fields after a select query

--- src/Snap/Snaplet/Auth/Backends/Hdbc.hs
+++ src/Snap/Snaplet/Auth/Backends/Hdbc.hs
@@ -30,8 +30,9 @@ initHdbcAuthManager
   -> AuthTable
   -> SnapletInit b (AuthManager b)
 initHdbcAuthManager s l conn tbl =
-  makeSnaplet "HdbcAuthManager" "A snaplet providing user authentication using an HDBC backend"
-    Nothing $ liftIO $ do
+  makeSnaplet  "HdbcAuthManager"
+               "A snaplet providing user authentication using an HDBC backend"
+               Nothing $ liftIO $ do
   key <- getKey (asSiteKey s)
   return AuthManager {
       backend = HdbcAuthManager conn tbl
@@ -117,7 +118,7 @@ instance IAuthBackend HdbcAuthManager where
             "INSERT INTO " ++ tblName tbl ++ " (" ++
             intercalate "," (map (\f -> f tbl)  colLst)
             ++ ") VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
-          mkStmt (Just uid) conn'' = prepare conn'' $
+          mkStmt (Just _) conn'' = prepare conn'' $
             "UPDATE " ++ tblName tbl ++ " SET (" ++
             intercalate "," (map (\f -> f tbl ++ " = ?")  colLst)
             ++ ") WHERE " ++ colId tbl ++ " = ?"
@@ -164,7 +165,8 @@ instance IAuthBackend HdbcAuthManager where
 mkSelect :: AuthTable -> String -> String
 mkSelect at whr = "SELECT * FROM " ++ tblName at ++ " WHERE " ++ whr ++ " = ? "
 
-query :: HdbcAuthManager -> (AuthTable -> String) -> [SqlValue] -> IO (Maybe AuthUser)
+query  ::  HdbcAuthManager -> (AuthTable -> String) -> [SqlValue]
+       ->  IO (Maybe AuthUser)
 query (HdbcAuthManager conn tbl) col vals = withTransaction conn $ \conn' -> do
   stmt  <- prepare conn' (mkSelect tbl $ col tbl)
   _     <- execute stmt vals
@@ -172,27 +174,30 @@ query (HdbcAuthManager conn tbl) col vals = withTransaction conn $ \conn' -> do
   case res of
     Nothing -> return Nothing
     Just mp -> return $ Just mkUser
-                 where rdSql mp' k = fromSql $ mp' DM.! k
-                 -- TODO: This is all very incomplete. We need to persist more and update various counters
-                       mkUser = AuthUser {
-                                  userId = Just . UserId $ rdSql mp $ colId tbl
-                                , userLogin = rdSql mp $ colLogin tbl
-                                , userPassword = Just . Encrypted $ rdSql mp $ colPassword tbl
-                                , userActivatedAt = Nothing -- :: Maybe UTCTime
-                                , userSuspendedAt = Nothing -- :: Maybe UTCTime
-                                , userRememberToken = Nothing -- :: Maybe Text
-                                , userLoginCount = 0 -- :: Int
-                                , userFailedLoginCount = 0 -- :: Int
-                                , userLockedOutAt = Nothing -- :: Maybe UTCTime
-                                , userCurrentLoginAt = Nothing -- :: Maybe UTCTime
-                                , userLastLoginAt = Nothing -- :: Maybe UTCTime
-                                , userCurrentLoginIp = Nothing -- :: Maybe ByteString
-                                , userLastLoginIp = Nothing -- :: Maybe ByteString
-                                , userCreatedAt = Nothing -- :: Maybe UTCTime
-                                , userUpdatedAt = Nothing -- :: Maybe UTCTime
-                                , userRoles = [] -- :: [Role]
-                                , userMeta = HM.empty -- :: HashMap Text Value
-                                }
+               where colLU col' = mp DM.! col' tbl
+                     rdSql' = rdSql'
+                     rdSql con col' =
+                       case colLU col' of
+                         SqlNull  -> Nothing
+                         x        -> Just . con $ fromSql x
+                     mkUser = AuthUser {
+                                   userId = rdSql UserId colId
+                                ,  userLogin = fromSql $ colLU colLogin
+                                ,  userPassword = rdSql Encrypted colPassword
+                                ,  userActivatedAt = rdSql' colActivatedAt
+                                ,  userSuspendedAt = rdSql' colSuspendedAt
+                                ,  userRememberToken = rdSql' colRememberToken
+                                ,  userLoginCount = fromSql $ colLU colLoginCount
+                                ,  userFailedLoginCount = fromSql $ colLU colFailedLoginCount
+                                ,  userLockedOutAt = rdSql' colLockedOutAt
+                                ,  userCurrentLoginAt = rdSql' colCurrentLoginAt
+                                ,  userLastLoginAt = rdSql' colLastLoginAt
+                                ,  userCurrentLoginIp = rdSql' colCurrentLoginIp
+                                ,  userLastLoginIp = rdSql' colLastLoginIp
+                                ,  userCreatedAt = rdSql' colCreatedAt
+                                ,  userUpdatedAt = rdSql' colUpdatedAt
+                                ,  userRoles = [] -- :: [Role] TODO
+                                ,  userMeta = HM.empty } -- :: HashMap Text Value TODO
 
 {- class IAuthBackend r where-}
 {-   save :: r -> AuthUser -> IO AuthUser-}
